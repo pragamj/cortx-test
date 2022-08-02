d6bc9cd366d6696d4b700cc0028346dbf5403629
pragam <pragam.jain@seagate.com>
2022-08-02 10:36:25 +0530

    Changed label
    Signed-off-by: pragam <pragam.jain@seagate.com>

--- scripts/cicd_k8s_cortx_deploy/create_db_entry.py
+++ scripts/cicd_k8s_cortx_deploy/create_db_entry.py
@@ -133,7 +133,7 @@ def main():
             jira_obj = jira_utils.JiraTask(jira_id, jira_pswd)
             te_details = jira_obj.get_issue_details(test_exe_no)
             test_env = te_details.fields.customfield_21006
-            print("TEST ENV", test_env[0].split("_")[0:1])
+            print("TEST ENV", test_env[0].split("_")[0:2])
             nodes_cnt = w2n.word_to_num(test_env[0].split("_")[0])
             print("WORKER NODE COUNT FOR ADDING DB ENTRY", nodes_cnt)
         else:
